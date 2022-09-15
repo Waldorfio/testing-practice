4ee7f9428bd5d77250733398bc29a346d245eb98
Waldorfio <walid.hanifi707@gmail.com>
2022-09-15 19:34:56 +1000

    Add function and tests

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,17 +1,25 @@
 function cipher (string, index) {
   let newString = ''
-  const alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  const alphU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
     'W', 'X', 'Y', 'Z']
+  const alphL = []
+  for (let k = 0; k < alphU.length; k++) {
+    alphL.push(alphU[k].toLowerCase())
+  }
 
   for (let j = 0; j < string.length; j++) {
-    for (let i = 0; i < alph.length - 1; i++) {
-      if (alph[i].toUpperCase() === string[j].toUpperCase()) {
-        newString = newString + alph[i + index]
+    for (let i = 0; i < alphU.length - 1; i++) {
+      if (alphU[i] === string[j]) {
+        newString = newString + alphU[i + index]
+        continue
+      }
+      if (alphL[i] === string[j]) {
+        newString = newString + alphL[i + index]
+        continue
       }
     }
   }
-  console.log(newString)
   return newString
 }
-module.exports = cipher;
+module.exports = cipher
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,5 +1,17 @@
 const cipher = require('./caesarCipher');
 
 test('abcd should be BCDE', () => {
-    expect(cipher('ABCD',1)).toBe('BCDE');
+    expect(cipher('abcd',1)).toBe('bcde');
+});
+
+test('abcd should be BCDE', () => {
+    expect(cipher('aBcD',1)).toBe('bCdE');
+});
+
+test('abcd should be BCDE', () => {
+    expect(cipher('abcd',2)).toBe('cdef');
+});
+
+test('abcd should be BCDE', () => {
+    expect(cipher('ab,cd',2)).toBe('cd,ef');
 });
